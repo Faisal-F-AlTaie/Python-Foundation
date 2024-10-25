80ab19e95aa2d39471af55504eb61e47257d1028
Faisal Al-Taie <faisal.faltaie@gmail.com>
2024-10-24 23:39:27 -0600

    Update and rename RandCoinFlip_matlib.py to 9.RandCoinFlip_matlib.py

--- 9.RandCoinFlip_matlib.py
+++ 9.RandCoinFlip_matlib.py
@@ -1,6 +1,7 @@
 import random
 import matplotlib.pyplot as plt  # Corrected import statement
 
+# had to move this outside the function to make it accisable to the new function 
 options = ["heads", "tails"]
 
 # Function for randomly generating heads or tails upon coin flip
@@ -24,10 +25,10 @@ def flip_count(num_flips):
 num_flips = int(input("Enter the number of flips you want to do: "))
 heads, tails = flip_count(num_flips)
 
-# Print the results
+# Print the results in a nice statement 
 print("Heads: " + str(heads) + " Tails: " + str(tails))
 
-# Plotting Data
+# Plotting the Data by setting up graph characteristics 
 labels = ["Heads", "Tails"]
 counts = [heads, tails]
 
