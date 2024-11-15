cde5e703d7447eb268af393672f809af5ac1241f
Faisal Al-Taie <faisal.faltaie@gmail.com>
2024-11-14 23:03:38 -0800

    Update Z.ContactManagment.py

--- Z.ContactManagment.py
+++ Z.ContactManagment.py
@@ -4,15 +4,18 @@
 # 3. Search for contact by name
 # 4. Delete a contact
 
+# The data base where all contacts are stored (will append option moving forward)
 contacts = []
 
+# The first thing to appear when ran and will show you what you can do. Its best to start by adding a contact
 def display_options():
     print("Welcome to the contact managment system")
     print("1. Add New Contact")
     print("1. View All Contacts")
     print("3. Search Contact")
     print("4. Delete Contact")
-    
+
+# when you press 1 in the display options, this function will appear and will ask you for 3 contact inputs before adding it (append) to the contacts data base
 def add_contact():
     name = input("Enter Name: ")
     phone = input("Enter Phone: ")
@@ -20,14 +23,16 @@ def add_contact():
     contact = {"name": name, "phone": phone, "email": email}
     contacts.append(contact)
     print("Contact has now been added to the data base :)")
-    
+
+# function to see your contact printed in a nice format
 def view_contacts():
     if not contacts:
         print("Sorry no conatcts were found in our data base")
         return
     for contact in contacts:
         print(f"Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
-        
+
+# function that allows you to serach for contacts within the data base (list)
 def search_contact():
     name = input("Enter the name to serach data base: ")
     found = False
@@ -38,7 +43,9 @@ def search_contact():
             break
     if not found:
         print("Contact not found.")
-        
+
+
+# allows you to remove contacts in the data base
 def delete_contact():
     name = input("Enter the name of the contact to delete: ")
     for contact in contacts:
@@ -48,6 +55,7 @@ def delete_contact():
             return
     print("Contact not found.")
 
+# allows the options to contiue to respawn after a function has been ran
 def main():
     while True:
         display_options()
